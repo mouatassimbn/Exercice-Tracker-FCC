099280559b5731235e6738dd41f65ee1c64f226f
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-01-07 14:21:12 -0400

    Initialized api routes managing file

---
+++ routes/api.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+//TODO: Register needed api routes
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,8 +9,9 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
+//Routes
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
