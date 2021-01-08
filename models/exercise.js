995dc3f467e53cb1a0d624c49547e13b0779a1b1
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-01-07 22:36:33 -0400

    Implemented needed crud op in exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -1,3 +1,4 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Exercise {
@@ -5,7 +6,7 @@ class Exercise {
     this.userId = userId;
     this.description = description;
     this.duration = duration;
-    this.date = date;
+    this.date = Date.parse(date);
   }
 
   save() {
@@ -15,6 +16,63 @@ class Exercise {
       .insertOne(this)
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
+  }
+
+  static fetchAll() {
+    const db = getDb();
+
+    return db
+      .collection("exercises")
+      .find()
+      .toArray()
+      .then((exercises) => {
+        return exercises;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static findById(exeId) {
+    const db = getDb();
+
+    db.collection("exercises")
+      .find({ _id: new mongodb.ObjectId(exeId) })
+      .next()
+      .then((exercise) => {
+        return exercise;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static findByUserId(userId) {
+    const db = getDb();
+
+    db.collection("exercises")
+      .find({ userId: userId })
+      .next()
+      .then((exercise) => {
+        return exercise;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static find(userId = "", from = "", to = "", limit = "") {
+    const validUserId = userId || "";
+    const validFrom = Date.parse(from) || "";
+    const validTo = Date.parse(to) || "";
+    const validLimit = limit || "";
+
+    const db = getDb();
+
+    db.collection("exercises").find({
+      userId: validUserId,
+      date: { $gt: validFrom, $lt: validTo },
+      duration: validLimit,
+    })
+    .next()
+    .then((exercise) => {
+      return exercise;
+    })
+    .catch((err) => console.log(err));
   }
 }
 
